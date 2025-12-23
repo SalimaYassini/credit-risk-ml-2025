4748838ed5cc4484f9645dcef2eecedd830798c2
SalimaYassini <safia142001@yahoo.fr>
2025-12-23 13:46:44 +0100

    Update app.py

--- app.py
+++ app.py
@@ -569,7 +569,7 @@ if page == "Simulation client":
     # HISTORIQUE PAR CLIENT
     # ===================================================================
     st.markdown("### Historique par client & Export Excel")
-        if st.session_state.get("save_now", False):
+    if st.session_state.get("save_now", False):
         niveau = "FAIBLE"
         if st.session_state.last_score_ajuste >= 0.40:
             niveau = "TRÈS ÉLEVÉ"
@@ -696,6 +696,7 @@ st.markdown("""
 st.sidebar.markdown("---")
 st.sidebar.markdown("**© Salima Yassini 2025 – Tous droits réservés**")
 st.sidebar.markdown("**safia142001@yahoo.fr • 07 78 24 78 49**")
+
 
 
 
